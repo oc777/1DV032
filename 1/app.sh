4083982436bf028eed71cdcfbfe19d1391037129
Olga Christensen <oc222ba@student.lnu.se>
2016-10-16 18:27:39 +0200

    Update app.sh

--- 1/app.sh
+++ 1/app.sh
@@ -1,17 +1,21 @@
 #!bin/bash
 
 #create new app
+echo "create new app"
 rails new newapp
 
 cd newapp
 
 #Bundle all the gems
+echo "Bundle all the gems"
 bundle install
 
 #generate scaffold
+echo "generate scaffold"
 rails generate scaffold HighScore game:string score:integer
 
 #Create and migrate the default sqlite3 
+echo "db create and migrate"
 #rails db:migrate
 rake db:migrate
 
