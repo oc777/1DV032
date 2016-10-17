3d482101aabe385f143e3631c695a2fef0aa67d8
Olga Christensen <oc222ba@student.lnu.se>
2016-10-17 11:30:04 +0200

    Update ruby.sh

--- 1/ruby.sh
+++ 1/ruby.sh
@@ -14,9 +14,9 @@ sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libread
 #Install RVM:
 echo "install rvm"
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 > /dev/null 2>&1
-\curl -sSL https://get.rvm.io | bash -s stable > /dev/null 2>&1
 
 echo progress-bar >> ~/.curlrc
+\curl -sSL https://get.rvm.io | bash -s stable > /dev/null 2>&1
 
 #load RVM
 source ~/.rvm/scripts/rvm
