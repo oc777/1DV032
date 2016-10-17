62a63ffbd9c64c9c8a940986c7f72d00079202bb
Olga Christensen <oc222ba@student.lnu.se>
2016-10-17 10:59:55 +0200

    Update ruby.sh

--- 1/ruby.sh
+++ 1/ruby.sh
@@ -8,13 +8,16 @@ sudo apt-get update > /dev/null 2>&1
 sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev > /dev/null 2>&1
 
 #For a progress bar when downloading RVM / Rubies:
-echo progress-bar >> ~/.curlrc
+
 
 
 #Install RVM:
 echo "install rvm"
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 > /dev/null 2>&1
 \curl -sSL https://get.rvm.io | bash -s stable > /dev/null 2>&1
+
+echo progress-bar >> ~/.curlrc
+
 #load RVM
 source ~/.rvm/scripts/rvm
 
@@ -22,6 +25,9 @@ source ~/.rvm/scripts/rvm
 #install ruby
 echo "install ruby"
 rvm install 2.3.1 > /dev/null 2>&1
+
+echo progress-bar >> ~/.curlrc
+
 rvm use 2.3.1 --default > /dev/null 2>&1
 
 
