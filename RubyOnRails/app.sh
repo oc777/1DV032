5e46cd490a7d1b06d216ca3bb6745065ed03447b
Olga Christensen <oc222ba@student.lnu.se>
2016-10-21 16:15:27 +0200

    correct app file

--- RubyOnRails/app.sh
+++ RubyOnRails/app.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
 #create new app
-echo "create hours app"
+echo "create new app"
 cd /vagrant
+rails new newapp
 
-git clone https://github.com/DefactoSoftware/Hours.git
-cd ./Hours
+cd newapp
 
-./bin/setup
+#Bundle all the gems
+echo "Bundle all the gems"
+bundle install
 
+#generate scaffold
+echo "generate scaffold"
+rails generate scaffold HighScore game:string score:integer
+
+#Create and migrate the default sqlite3 
+echo "db create and migrate"
+#rails db:migrate
+rake db:migrate
 
 #starting server
 echo "starting server"
